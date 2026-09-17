34b28e80e85c6529791f3c5d016dc78a12d67e43
Neelam Raikwar <neelam.raikwar.234303@gmail.com>
2026-09-17 12:55:14 +0530

    first commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import "./App.css";
+import { Routes, Route } from 'react-router-dom'; 
+import Plantora from './Pages/Plantora'
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return 
+  <>
+  <Routes>
+    <Route path="/" element={<Plantora/>}></Route>
+  </Routes>
+  
+
+  </>;
+}
+
+export default App;
---
+++ vite-project/src/Pages/Plantora.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Plantora = () => {
+  return (
+    <div>
+      <h1>Plantora</h1>
+    </div>
+  );
+}
+
+export default Plantora; 
